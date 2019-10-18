39122b9895a31519b2fae21071f433aee9d39f53
zerstorers <lemaire-maxime@hotmail.fr>
2019-10-18 16:46:51 +0200

    On y est preque !

--- scriptSchnake.js
+++ scriptSchnake.js
@@ -6,9 +6,9 @@ jQuery(document).ready(function () {
     var schnake = $("<div id='schnake'>");
     var fruit = $("<div id='fruit'>");
 
-    $("body").append(schnake);
-    $("body").append(fruit);
     $("body").append(board);
+    $("#board").append(schnake);
+    $("#board").append(fruit);
 
     //  random position fruit
 
@@ -17,65 +17,83 @@ jQuery(document).ready(function () {
 
     //  move schnake down infinite
 
-    var snakeDirection = "top"
-    var snakeDirectionValue = '+=5px'
+    var snakeDirection = "top";
+    var snakeDirectionValue = '+=20px';
 
     setInterval(function () {
 
         $("#schnake").css(snakeDirection, snakeDirectionValue);
         $("#schnake").css('transitionDuration = 0.5s');
 
+        // limit of the board
+
+        if ($("#schnake").offset().top <= $(board).scrollTop()) {
+            alert("try again");
+        }
+        else if ($("#schnake").offset().top > ($(board).height() - 130)) {
+            alert("try again");
+        }
+        else if ($("#schnake").offset().left < $(board).scrollLeft()) {
+            $("#schnake").css("left", "100%").css("left", "-=101px");
+            alert("try again");
+        }
+        else if ($("#schnake").offset().left > $(board).width() - 100) {
+            alert("try again");
+        }
+      
+        function verifPosition(){
+            FruitTop = $(board).scrollTop() + $("#fruit").offset().top
+            FruitLeft = $(board).scrollLeft() + $("#fruit").offset().left
+            SnakeTop = $(board).scrollTop() + $("#schnake").offset().top;
+            SnakeLeft = $(board).scrollLeft() + $("#schnake").offset().left;
+            if (SnakeTop == FruitTop && SnakeLeft == FruitLeft){
+                alert("gg")
+            }
+            
+        }
+    
+        verifPosition();
+        
     }, 100);
+    
 
-    // limit of the board
 
-    if ($("#schnake").offset().top <= $(board).scrollTop()) {
-        // $("#schnake").css("top", "100%").css("top", "-=101px");
-        alert("prout")
-    }
-    else if ($("#schnake").offset().top > ($(board).height() - 130)) {
-        // $("#schnake").css("top", "1px");
-        alert("prout")
-    }
-    else if ($("#schnake").offset().left < $(board).scrollLeft()) {
-        // $("#schnake").css("left", "100%").css("left", "-=101px");
-        alert("prout")
-    }
-    else if ($("#schnake").offset().left > $(board).width() - 100) {
-        // $("#schnake").css("left", "1px");
-        alert("prout")
-    }
+    
+
+
+
+
 
     // set movement pad
 
     $("body").keydown(function (evt) {
 
         if (evt.keyCode == 40) {//bas
-            snakeDirection = "top"
-            snakeDirectionValue = '+=5px'
-            $("#schnake").css('background-image', "url(schnake.png)")
+            snakeDirection = "top";
+            snakeDirectionValue = '+=20px';
+            $("#schnake").css('background-image', "url(schnake.png)");
 
 
         }
         else if (evt.keyCode == 38) {//haut
-            snakeDirection = "top"
-            snakeDirectionValue = '-=5px'
-            $("#schnake").css('background-image', "url(schnakeup.png)")
-            // $("#schnake").attr('class' , 'schnakeup'); // for replace initial width and height don't work 
+            snakeDirection = "top";
+            snakeDirectionValue = '-=20px';
+            $("#schnake").css('background-image', "url(schnakeup.png)");
+            $("#schnake").attr('class', 'schnakeup'); // for replace initial width and height don't work 
 
 
         }
         else if (evt.keyCode == 37) {//gauche
-            snakeDirection = "left"
-            snakeDirectionValue = '-=5px'
-            $("#schnake").css('background-image', "url(schnakeleft.png)")
-            
+            snakeDirection = "left";
+            snakeDirectionValue = '-=20px';
+            $("#schnake").css('background-image', "url(schnakeleft.png)");
+
 
         }
         else if (evt.keyCode == 39) {//droite
-            snakeDirection = "left"
-            snakeDirectionValue = '+=5px'
-            $("#schnake").css('background-image', "url(schnakeright.png)")
+            snakeDirection = "left";
+            snakeDirectionValue = '+=20px';
+            $("#schnake").css('background-image', "url(schnakeright.png)");
         }
 
     });
